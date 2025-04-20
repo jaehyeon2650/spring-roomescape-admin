c7c2ab3f6c1b96477803996cbb6560bcb4592a7b
wogus <ind07152@naver.com>
2025-04-20 21:31:09 +0900

    test: jdbcTemplate 전체 기능 테스트

--- src/test/java/roomescape/JdbcTemplateTest.java
+++ src/test/java/roomescape/JdbcTemplateTest.java
@@ -7,9 +7,12 @@ import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class JdbcTemplateTest {
 
     @Autowired
