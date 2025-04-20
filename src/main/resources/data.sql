1f29a0746d1c1092f3dbcea9697f68d68ef436c7
wogus <ind07152@naver.com>
2025-04-20 21:28:29 +0900

    docs(resource): data.sql 추가
    - 초기 데이터 삽입

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into reservation(name, date, time) values ('브라운','2024-04-01','10:00');
+insert into reservation(name, date, time) values ('솔라','2024-04-01','11:00');
+insert into reservation(name, date, time) values ('브리','2024-04-02','14:00');
