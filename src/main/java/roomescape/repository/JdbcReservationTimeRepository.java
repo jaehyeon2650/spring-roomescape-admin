da414616f26c580f341bf68bf455314620951e42
wogus <ind07152@naver.com>
2025-04-23 20:48:17 +0900

    feat(repository): ReservationTime 레포지토리 생성
    - ReservationTime 추가 기능
    - ReservationTime 조회 기능
    - ReservationTime 삭제 기능
    - 전체 ReservationTime 조회 기능

---
+++ src/main/java/roomescape/repository/JdbcReservationTimeRepository.java
@@ -0,0 +1,60 @@
+package roomescape.repository;
+
+import java.sql.PreparedStatement;
+import java.time.LocalTime;
+import java.util.List;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import roomescape.model.ReservationTime;
+
+@Repository
+public class JdbcReservationTimeRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcReservationTimeRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Long addReservationTime(ReservationTime reservationTime) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        String time = reservationTime.getStartAt().toString();
+        jdbcTemplate.update((connection) -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    "insert into reservation_time(start_at) values(?)", new String[]{"id"});
+            ps.setString(1, time);
+            return ps;
+        }, keyHolder);
+        return keyHolder.getKey().longValue();
+    }
+
+    public ReservationTime findReservationTimeById(Long id) {
+        return jdbcTemplate.queryForObject("select * from reservation_time where id = ?", reservationTimeRowMapper(),
+                id);
+    }
+
+    public List<ReservationTime> findAllReservationTimes() {
+        return jdbcTemplate.query("select * from reservation_time", reservationTimeRowMapper());
+    }
+
+    public void deleteById(Long id) {
+        int deleteCount = jdbcTemplate.update("delete from reservation_time where id = ?", id);
+
+        if (deleteCount == 0) {
+            throw new EmptyResultDataAccessException("해당 ID가 존재하지 않습니다.", 1);
+        }
+    }
+
+    private RowMapper<ReservationTime> reservationTimeRowMapper() {
+        return (rs, rowNum) -> {
+            Long id = rs.getLong("id");
+            String startAt = rs.getString("start_at");
+            LocalTime time = LocalTime.parse(startAt);
+            return new ReservationTime(id, time);
+        };
+    }
+}
