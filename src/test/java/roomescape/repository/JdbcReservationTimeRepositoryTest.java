70627eecffdfbfdcbeca0b6dda87e23548274c06
wogus <ind07152@naver.com>
2025-04-23 21:44:50 +0900

    test(repository): ReservationTime 레포지토리 테스트

---
+++ src/test/java/roomescape/repository/JdbcReservationTimeRepositoryTest.java
@@ -0,0 +1,95 @@
+package roomescape.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalTime;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import roomescape.model.ReservationTime;
+
+@JdbcTest
+class JdbcReservationTimeRepositoryTest {
+
+    private JdbcReservationTimeRepository repository;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void beforeEach() {
+        jdbcTemplate.execute("DROP TABLE reservation_time IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE reservation_time("
+                + " id BIGINT NOT NULL AUTO_INCREMENT,"
+                + " start_at VARCHAR(255) NOT NULL,"
+                + " PRIMARY KEY (id));");
+        repository = new JdbcReservationTimeRepository(jdbcTemplate);
+    }
+
+    @Test
+    @DisplayName("reservationTime 추가 테스트")
+    void addReservationTimeTest() {
+        // given
+        ReservationTime time = ReservationTime.createReservationTimeWithoutId(LocalTime.of(11, 0));
+        // when
+        Long id = repository.addReservationTime(time);
+        // then
+        ReservationTime findTime = repository.findReservationTimeById(id);
+        assertThat(findTime).isNotNull();
+        assertThat(findTime.getStartAt()).isEqualTo(time.getStartAt());
+    }
+
+    @Test
+    @DisplayName("reservationTime 조회 테스트")
+    void findReservationTimeById() {
+        // given
+        ReservationTime time1 = ReservationTime.createReservationTimeWithoutId(LocalTime.of(11, 0));
+        ReservationTime time2 = ReservationTime.createReservationTimeWithoutId(LocalTime.of(12, 0));
+        ReservationTime time3 = ReservationTime.createReservationTimeWithoutId(LocalTime.of(10, 0));
+        repository.addReservationTime(time1);
+        repository.addReservationTime(time2);
+        Long time3Id = repository.addReservationTime(time3);
+        // when
+        ReservationTime findTime = repository.findReservationTimeById(time3Id);
+        // then
+        assertThat(findTime.getStartAt()).isEqualTo(time3.getStartAt());
+    }
+
+    @Test
+    @DisplayName("전체 reservationTime 조회 테스트")
+    void findAllReservationTimeTest() {
+        // given
+        ReservationTime time1 = ReservationTime.createReservationTimeWithoutId(LocalTime.of(11, 0));
+        ReservationTime time2 = ReservationTime.createReservationTimeWithoutId(LocalTime.of(12, 0));
+        ReservationTime time3 = ReservationTime.createReservationTimeWithoutId(LocalTime.of(10, 0));
+        repository.addReservationTime(time1);
+        repository.addReservationTime(time2);
+        repository.addReservationTime(time3);
+        // when
+        List<ReservationTime> allReservationTimes = repository.findAllReservationTimes();
+        // then
+        assertThat(allReservationTimes).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("reservationTime 삭제 테스트")
+    void deleteReservationTimeTest() {
+        // given
+        ReservationTime time1 = ReservationTime.createReservationTimeWithoutId(LocalTime.of(11, 0));
+        ReservationTime time2 = ReservationTime.createReservationTimeWithoutId(LocalTime.of(12, 0));
+        ReservationTime time3 = ReservationTime.createReservationTimeWithoutId(LocalTime.of(10, 0));
+        repository.addReservationTime(time1);
+        repository.addReservationTime(time2);
+        Long time3Id = repository.addReservationTime(time3);
+        // when
+        repository.deleteById(time3Id);
+        // then
+        List<ReservationTime> allReservationTimes = repository.findAllReservationTimes();
+        assertThat(allReservationTimes).hasSize(2);
+        assertThat(allReservationTimes.get(0).getStartAt()).isNotEqualTo(time3.getStartAt());
+        assertThat(allReservationTimes.get(1).getStartAt()).isNotEqualTo(time3.getStartAt());
+    }
+}
