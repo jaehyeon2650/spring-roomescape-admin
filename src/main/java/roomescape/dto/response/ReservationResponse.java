1d3326f4fa01eb3f9c22af5bc5e541d743d05531
wogus <ind07152@naver.com>
2025-04-24 11:52:25 +0900

    refactor(dto): ReservationResponse dto 구성 변경

--- src/main/java/roomescape/dto/response/ReservationResponse.java
+++ src/main/java/roomescape/dto/response/ReservationResponse.java
@@ -1,6 +1,6 @@
 package roomescape.dto.response;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import roomescape.model.Reservation;
@@ -9,18 +9,17 @@ public record ReservationResponse(
         Long id,
         String name,
         String date,
-        String time
+        ReservationTimeResponse time
 ) {
 
     public static ReservationResponse from(Reservation reservation) {
-        LocalDateTime dateTime = reservation.getReservationTime();
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        LocalDate date = reservation.getReservationDate();
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         return new ReservationResponse(
                 reservation.getId(),
                 reservation.getName(),
-                dateTime.toLocalDate().format(dateFormatter),
-                dateTime.toLocalTime().format(timeFormatter));
+                date.format(dateFormatter),
+                ReservationTimeResponse.from(reservation.getReservationTime()));
     }
 
     public static List<ReservationResponse> from(List<Reservation> reservations) {
