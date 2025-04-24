56ec0d730383fa36e317014ec65c0fbaa40f4724
wogus <ind07152@naver.com>
2025-04-24 16:35:43 +0900

    docs(sql): sql 관련 docs 변경
    - 초기화 변경
    - 스키마 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into reservation(name, date, time) values ('브라운','2024-04-01','10:00');
-insert into reservation(name, date, time) values ('솔라','2024-04-01','11:00');
-insert into reservation(name, date, time) values ('브리','2024-04-02','14:00');
+insert into reservation_time(start_at) values('10:00');
+insert into reservation_time(start_at) values('11:00');
+insert into reservation_time(start_at) values('12:00');
