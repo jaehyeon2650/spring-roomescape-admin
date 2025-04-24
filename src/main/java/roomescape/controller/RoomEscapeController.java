64e986573c4889d240a2d526f230e98ef101eb1f
wogus <ind07152@naver.com>
2025-04-24 16:34:40 +0900

    feat(service): 서비스 계층 추가

--- src/main/java/roomescape/controller/RoomEscapeController.java
+++ src/main/java/roomescape/controller/RoomEscapeController.java
@@ -8,42 +8,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.request.ReservationRequest;
 import roomescape.dto.request.ReservationTimeAddRequest;
 import roomescape.dto.response.ReservationResponse;
 import roomescape.dto.response.ReservationTimeResponse;
-import roomescape.model.ReservationTime;
-import roomescape.repository.JdbcReservationTimeRepository;
-import roomescape.repository.ReservationRepository;
+import roomescape.service.ReservationService;
 
 @RestController
 public class RoomEscapeController {
 
-    private final ReservationRepository repository;
-    private final JdbcReservationTimeRepository timeRepository;
+    private final ReservationService reservationService;
 
-    public RoomEscapeController(ReservationRepository repository, JdbcReservationTimeRepository timeRepository) {
-        this.repository = repository;
-        this.timeRepository = timeRepository;
+    public RoomEscapeController(ReservationService reservationService) {
+        this.reservationService = reservationService;
     }
 
     @GetMapping("/reservations")
     public List<ReservationResponse> findReservations() {
-        return ReservationResponse.from(repository.findAll());
+        return reservationService.findAllReservations();
     }
 
     @PostMapping("/reservations")
     public ReservationResponse addReservation(@RequestBody ReservationRequest request) {
-        Long id = repository.add(request.dtoToReservationWithoutId());
-        return ReservationResponse.from(repository.findById(id));
+        return reservationService.createReservation(request);
     }
 
     @DeleteMapping("/reservations/{id}")
     public ResponseEntity<Void> deleteReservation(@PathVariable("id") Long id) {
         try {
-            repository.deleteById(id);
+            reservationService.deleteReservation(id);
             return ResponseEntity.ok().build();
         } catch (EmptyResultDataAccessException e) {
             return ResponseEntity.badRequest().build();
@@ -52,21 +46,18 @@ public class RoomEscapeController {
 
     @PostMapping("/times")
     public ReservationTimeResponse addReservationTime(@RequestBody ReservationTimeAddRequest request) {
-        Long id = timeRepository.addReservationTime(request.dtoToReservationTimeWithoutId());
-        ReservationTime reservationTime = timeRepository.findReservationTimeById(id);
-        return ReservationTimeResponse.from(reservationTime);
+        return reservationService.createReservationTime(request);
     }
 
     @GetMapping("/times")
     public List<ReservationTimeResponse> findAllReservationTimes() {
-        List<ReservationTime> reservationTimes = timeRepository.findAllReservationTimes();
-        return ReservationTimeResponse.from(reservationTimes);
+        return reservationService.findAllReservationTimes();
     }
 
     @DeleteMapping("/times/{id}")
     public ResponseEntity<Void> deleteReservationTime(@PathVariable("id") Long id) {
         try {
-            timeRepository.deleteById(id);
+            reservationService.deleteReservationTime(id);
             return ResponseEntity.ok().build();
         } catch (EmptyResultDataAccessException e) {
             return ResponseEntity.badRequest().build();
---
+++ src/main/java/roomescape/service/ReservationService.java
@@ -0,0 +1,57 @@
+package roomescape.service;
+
+import java.time.LocalTime;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import roomescape.dto.request.ReservationRequest;
+import roomescape.dto.request.ReservationTimeAddRequest;
+import roomescape.dto.response.ReservationResponse;
+import roomescape.dto.response.ReservationTimeResponse;
+import roomescape.model.Reservation;
+import roomescape.model.ReservationTime;
+import roomescape.repository.JdbcReservationTimeRepository;
+import roomescape.repository.ReservationRepository;
+import roomescape.repository.ReservationTimeRepository;
+
+@Service
+public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+    private final ReservationTimeRepository timeRepository;
+
+    public ReservationService(ReservationRepository reservationRepository,
+                              JdbcReservationTimeRepository timeRepository) {
+        this.reservationRepository = reservationRepository;
+        this.timeRepository = timeRepository;
+    }
+
+    public ReservationResponse createReservation(ReservationRequest request) {
+        ReservationTime reservationTime = timeRepository.findById(request.getTimeId());
+        Reservation reservationWithoutId = Reservation.createReservationWithoutId(request.getName(), request.getDate(),
+                reservationTime);
+        Reservation reservationEntity = reservationRepository.add(reservationWithoutId);
+        return ReservationResponse.from(reservationEntity);
+    }
+
+    public List<ReservationResponse> findAllReservations(){
+        return ReservationResponse.from(reservationRepository.findAll());
+    }
+
+    public void deleteReservation(Long id){
+        reservationRepository.deleteById(id);
+    }
+
+    public ReservationTimeResponse createReservationTime(ReservationTimeAddRequest request){
+        ReservationTime time = ReservationTime.createReservationTimeWithoutId(LocalTime.parse(request.getStartAt()));
+        ReservationTime timeEntity = timeRepository.add(time);
+        return ReservationTimeResponse.from(timeEntity);
+    }
+
+    public List<ReservationTimeResponse> findAllReservationTimes(){
+        return ReservationTimeResponse.from(timeRepository.findAllReservationTimes());
+    }
+
+    public void deleteReservationTime(Long id){
+        timeRepository.deleteById(id);
+    }
+}
