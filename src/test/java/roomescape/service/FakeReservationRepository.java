acd84149aae180b3009e8d68121744ecd3306f38
wogus <ind07152@naver.com>
2025-04-25 17:42:00 +0900

    test(repository): repository fake 객체 생성

---
+++ src/test/java/roomescape/service/FakeReservationRepository.java
@@ -0,0 +1,41 @@
+package roomescape.service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.dao.EmptyResultDataAccessException;
+import roomescape.model.Reservation;
+import roomescape.repository.ReservationRepository;
+
+public class FakeReservationRepository implements ReservationRepository {
+    private final List<Reservation> reservations;
+
+    private AtomicLong index = new AtomicLong(0);
+
+    public FakeReservationRepository() {
+        reservations = new ArrayList<>();
+    }
+
+    @Override
+    public List<Reservation> findAll() {
+        return Collections.unmodifiableList(reservations);
+    }
+
+    @Override
+    public Reservation add(Reservation reservation) {
+        long currentIndex = index.incrementAndGet();
+        Reservation reservationEntity = reservation.toEntity(currentIndex);
+        reservations.add(reservationEntity);
+        return reservationEntity;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        Reservation findReservation = reservations.stream()
+                .filter(reservation -> reservation.sameId(id))
+                .findAny()
+                .orElseThrow(() -> new EmptyResultDataAccessException("해당 ID가 존재하지 않습니다.", 1));
+        reservations.remove(findReservation);
+    }
+}
