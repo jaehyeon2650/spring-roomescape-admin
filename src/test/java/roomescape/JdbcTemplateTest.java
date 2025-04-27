5571231e045bd08d1b71271c320d4f983144f52a
wogus <ind07152@naver.com>
2025-04-27 19:06:29 +0900

    test: JdbcTemplateTest DirtiesContext 삭제

--- src/test/java/roomescape/JdbcTemplateTest.java
+++ src/test/java/roomescape/JdbcTemplateTest.java
@@ -9,10 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class JdbcTemplateTest {
 
     @Autowired
